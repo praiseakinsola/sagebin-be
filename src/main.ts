3110fe3508400d7bf815dd76747b2cb401c42ab1
Praise Akinsola <akinsolapraise1@gmail.com>
2026-02-17 01:31:11 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -13,6 +13,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.enableCors();
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT');
